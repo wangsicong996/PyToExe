fa200b5bebb273a91f09a9a75b28c203aedad2f2
eroge-69 <erogexunamed@gmail.com>
2025-10-13 03:03:47 +0700

    Upload Machinarium.py via PY to EXE tool

---
+++ python-files/Machinarium.py
@@ -0,0 +1,2 @@
+import webbrowser
+webbrowser.open('com.epicgames.launcher://apps/e52b4d8cbcfd45ad95eff800ccc59d93%3Ae3bc416f823d4a00900cd47c1eabfbc0%3Ace3fb6fe8fdf4c9ba649abc3e407debf?action=launch&silent=true')
